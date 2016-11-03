22e747233927cffa7970a3fb2528a231c2117f41
Adam Dipinto <adam.dipinto@gmail.com>
2016-11-03 18:30:24 +0000

    Modified actionWhenFound and findWaldo function in order to provide index at when Waldo is found.

--- callbackWaldo.js
+++ callbackWaldo.js
@@ -1,3 +1,16 @@
 // Lighthouse Labs - Callback Exercise //
 
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
 
+function actionWhenFound(num) {
+  console.log("Found Waldo at index " + num);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
